ceaa26e7e39c75c1440fabfc16a31a0838c7804f
Samira Vaeidi <131511478+Samira-V@users.noreply.github.com>
2024-08-31 21:22:40 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
+
   build: {
     rollupOptions: {
         output:{
             manualChunks(id) {
                 if (id.includes('node_modules')) {
                     return id.toString().split('node_modules/')[1].split('/')[0].toString();
-=======
+
     build: {
         rollupOptions: {
             output:{
@@ -19,13 +19,13 @@ export default defineConfig({
                     if (id.includes('node_modules')) {
                         return id.toString().split('node_modules/')[1].split('/')[0].toString();
                     }
->>>>>>> 8772a7cfcc1ce5e6181667d7025ffd6923ba4e66
+
                 }
             }
         }
     }
-<<<<<<< HEAD
+
 }
-=======
->>>>>>> 8772a7cfcc1ce5e6181667d7025ffd6923ba4e66
+
+
 })
